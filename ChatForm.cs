f1e79a306884c59c241f2ee7a574714b693f195e
developer119 <developer119korea@gmail.com>
2017-07-21 13:37:45 +0900

    Code Clear

--- ChatForm.cs
+++ ChatForm.cs
@@ -10,30 +10,36 @@ namespace MultiChatServer
 {
     public partial class ChatForm : Form
     {
-        delegate void AppendTextDelegate(Control ctrl, string s);
-        AppendTextDelegate _textAppender;
-        Socket mainSock;
-        IPAddress thisAddress;
+        public const int BufferSize = 1024;
 
-        private int _receiveSize = 0;
-        private int _packetSize = 0;
+        private delegate void AppendTextDelegate(Control ctrl, string s);
 
+        private List<Socket> _connectedClients = new List<Socket>();
         private List<byte> _byteList = new List<byte>();
         private Queue<Packet> _packetQueue = new Queue<Packet>();
 
+        private AppendTextDelegate _textAppender;
+        private Socket _mainSock;
+        private IPAddress _thisAddress;
+
+        private int _packetSize = 0;
 
         public ChatForm()
         {
             InitializeComponent();
 
-            mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            _mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
             _textAppender = new AppendTextDelegate(AppendText);
             _byteList.Clear();
+            _packetQueue.Clear();
         }
 
-        void AppendText(Control ctrl, string s)
+        private void AppendText(Control ctrl, string s)
         {
-            if (ctrl.InvokeRequired) ctrl.Invoke(_textAppender, ctrl, s);
+            if (ctrl.InvokeRequired)
+            {
+                ctrl.Invoke(_textAppender, ctrl, s);
+            }
             else
             {
                 string source = ctrl.Text;
@@ -41,7 +47,7 @@ namespace MultiChatServer
             }
         }
 
-        void OnFormLoaded(object sender, EventArgs e)
+        private void OnFormLoaded(object sender, EventArgs e)
         {
             IPHostEntry he = Dns.GetHostEntry(Dns.GetHostName());
 
@@ -50,19 +56,22 @@ namespace MultiChatServer
             {
                 if (addr.AddressFamily == AddressFamily.InterNetwork)
                 {
-                    thisAddress = addr;
+                    _thisAddress = addr;
                     break;
                 }
             }
 
             // 주소가 없다면..
-            if (thisAddress == null)
+            if (_thisAddress == null)
+            {
                 // 로컬호스트 주소를 사용한다.
-                thisAddress = IPAddress.Loopback;
+                _thisAddress = IPAddress.Loopback;
+            }
 
-            txtAddress.Text = thisAddress.ToString();
+            txtAddress.Text = _thisAddress.ToString();
         }
-        void BeginStartServer(object sender, EventArgs e)
+
+        private void BeginStartServer(object sender, EventArgs e)
         {
             int port;
             if (!int.TryParse(txtPort.Text, out port))
@@ -75,39 +84,35 @@ namespace MultiChatServer
 
             // 서버에서 클라이언트의 연결 요청을 대기하기 위해
             // 소켓을 열어둔다.
-            IPEndPoint serverEP = new IPEndPoint(thisAddress, port);
-            mainSock.Bind(serverEP);
-            mainSock.Listen(10);
+            IPEndPoint serverEP = new IPEndPoint(_thisAddress, port);
+            _mainSock.Bind(serverEP);
+            _mainSock.Listen(10);
 
             // 비동기적으로 클라이언트의 연결 요청을 받는다.
-            mainSock.BeginAccept(AcceptCallback, null);
+            _mainSock.BeginAccept(AcceptCallback, null);
 
             AppendText(txtHistory, "서버 시작 됐다");
         }
 
-        List<Socket> connectedClients = new List<Socket>();
-        void AcceptCallback(IAsyncResult ar)
+        private void AcceptCallback(IAsyncResult ar)
         {
             // 클라이언트의 연결 요청을 수락한다.
-            Socket client = mainSock.EndAccept(ar);
+            Socket client = _mainSock.EndAccept(ar);
 
             // 또 다른 클라이언트의 연결을 대기한다.
-            mainSock.BeginAccept(AcceptCallback, null);
+            _mainSock.BeginAccept(AcceptCallback, null);
 
-            AsyncObject obj = new AsyncObject(1024);
+            AsyncObject obj = new AsyncObject(BufferSize);
             obj.WorkingSocket = client;
 
             // 연결된 클라이언트 리스트에 추가해준다.
-            connectedClients.Add(client);
+            _connectedClients.Add(client);
 
             // 텍스트박스에 클라이언트가 연결되었다고 써준다.
             AppendText(txtHistory, string.Format("클라이언트 (@ {0})가 연결되었습니다.", client.RemoteEndPoint));
 
             // 클라이언트의 데이터를 받는다.
-
-
-            client.BeginReceive(obj.Buffer, 0, 1024, 0, StreamReceive, obj);
-
+            client.BeginReceive(obj.Buffer, 0, BufferSize, 0, StreamReceive, obj);
         }
 
         private void StreamReceive(IAsyncResult ar)
@@ -191,7 +196,7 @@ namespace MultiChatServer
 
             Packet packet = _packetQueue.Dequeue();
 
-            AsyncObject obj = new AsyncObject(1024);
+            AsyncObject obj = new AsyncObject(BufferSize);
             
             if(packet.PacketType.n == (int)PacketType.USER_INFO)
             {
@@ -220,29 +225,37 @@ namespace MultiChatServer
             AppendText(txtHistory, text);  //string.Format("[받음]{0}: {1}", ip, msg));
 
             // for을 통해 "역순"으로 클라이언트에게 데이터를 보낸다.
-            for (int i = connectedClients.Count - 1; i >= 0; i--)
+            for (int i = _connectedClients.Count - 1; i >= 0; i--)
             {
-                Socket socket = connectedClients[i];
+                Socket socket = _connectedClients[i];
                 //if (socket != obj.WorkingSocket) //@TODO: 일단 같은 소켓이라도 보내도록 하자
                 //{
-                try
-                {
-                    socket.Send(packet.ToBytes());
-                }
-                catch
-                {
-                    // 오류 발생하면 전송 취소하고 리스트에서 삭제한다.
-                    try { socket.Dispose(); } catch { }
-                    connectedClients.RemoveAt(i);
-                }
+                    try
+                    {
+                        socket.Send(packet.ToBytes());
+                    }
+                    catch
+                    {
+                        // 오류 발생하면 전송 취소하고 리스트에서 삭제한다.
+                        try
+                        {
+                            socket.Dispose();
+                        }
+                        catch(Exception ex)
+                        {
+
+                        }
+                        _connectedClients.RemoveAt(i);
+                    }
                 //}
             }
         }
 
-        void OnSendData(object sender, EventArgs e)
+        private void OnSendData(object sender, EventArgs e)
         {
+            return;
             // 서버가 대기중인지 확인한다.
-            if (!mainSock.IsBound)
+            if (!_mainSock.IsBound)
             {
                 MsgBoxHelper.Warn("서버가 실행되고 있지 않습니다!");
                 return;
@@ -258,59 +271,25 @@ namespace MultiChatServer
             }
 
             // 문자열을 utf8 형식의 바이트로 변환한다.
-            byte[] bDts = Encoding.UTF8.GetBytes(thisAddress.ToString() + '\x01' + tts);
+            byte[] bDts = Encoding.UTF8.GetBytes(_thisAddress.ToString() + '\x01' + tts);
 
             // 연결된 모든 클라이언트에게 전송한다.
-            for (int i = connectedClients.Count - 1; i >= 0; i--)
+            for (int i = _connectedClients.Count - 1; i >= 0; i--)
             {
-                Socket socket = connectedClients[i];
+                Socket socket = _connectedClients[i];
                 try { socket.Send(bDts); }
                 catch
                 {
                     // 오류 발생하면 전송 취소하고 리스트에서 삭제한다.
                     try { socket.Dispose(); } catch { }
-                    connectedClients.RemoveAt(i);
+                    _connectedClients.RemoveAt(i);
                 }
             }
 
             // 전송 완료 후 텍스트박스에 추가하고, 원래의 내용은 지운다.
-            AppendText(txtHistory, string.Format("[보냄]{0}: {1}", thisAddress.ToString(), tts));
+            AppendText(txtHistory, string.Format("[보냄]{0}: {1}", _thisAddress.ToString(), tts));
             txtTTS.Clear();
         }
 
-
-        private bool IsGetPacketSize(byte[] buff)
-        {
-            if (buff.Length < 2)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        private int GetPacketTotalSize(byte[] buff)
-        {
-            int result = 0;
-
-            if (IsGetPacketSize(buff))
-            {
-                byte[] size = new byte[2];
-                size[0] = buff[0];
-                size[1] = buff[1];
-                result = (int)Util.ByteArrToShort(size, 0);
-            }
-            return result;
-        }
-
-        private int GetPacketType(byte[] buff)
-        {
-            int result = 0;
-            int offset = 2; //앞에 바이트길이 헤더 2바이트
-
-            result = Util.ByteArrToInt(buff, offset);
-
-            return result;
-        }
     }
 }
